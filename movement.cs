3c05324799a069c4c217220230bb42cf46510304
lui2k <37487891+lui2k@users.noreply.github.com>
2018-10-21 16:17:36 +0100

    Update movement.cs
    -Added jump mechanic
    -Replaced Input.GetKey with Input.GetAxisRaw

--- movement.cs
+++ movement.cs
@@ -4,36 +4,66 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+    Rigidbody rb;
+    bool grounded;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.W))
-        {
-            transform.position += transform.forward * Time.deltaTime * 2f;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position -= transform.forward * Time.deltaTime * 2f;
-        }
-
-        if (Input.GetKey(KeyCode.A))
+		if(Input.GetAxisRaw("Horizontal") < 0)
         {
             transform.position -= transform.right * Time.deltaTime * 2f;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetAxisRaw("Horizontal") > 0)
         {
             transform.position += transform.right * Time.deltaTime * 2f;
         }
 
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            transform.position += transform.forward * Time.deltaTime * 2f;
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            transform.position -= transform.forward * Time.deltaTime * 2f;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+
+        //look around on Y-axis with mouse
         float mouseInput = Input.GetAxis("Mouse X");
         Vector3 lookhere = new Vector3(0, mouseInput, 0);
         transform.Rotate(lookhere);
+        Cursor.lockState = CursorLockMode.Locked; //locking mouse cursor to screen area
+    }
 
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * 150f);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.name == "Plane")
+        {
+            grounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Plane")
+        {
+            grounded = true;
+        }
     }
 }
